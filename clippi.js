c549cb1d585ea2cb35b160b59771994680dad2b6
Nick Steger <nsteger@iastate.edu>
2018-03-17 16:41:39 -0500

    Add getusers command

--- clippi.js
+++ clippi.js
@@ -1,6 +1,10 @@
+#!/usr/bin/env node
+
+'use strict';
+
 const program = require('commander');
-const http = require('http');
 const util = require('./lib/util');
+const ClippiRequest = require('./lib/clippiRequest');
 
 program
     .version('1.0.0', '-v, --version')
@@ -9,7 +13,18 @@ program
 program
     .command('getusers')
     .description('Get all users')
-    .action(() => util.getUsers());
+    .action(() =>  {
+        let cr = new ClippiRequest('http://localhost:3000/api/get-user/all');
+        cr.setContentType('application/json');
+        cr.setAuthHeader('bearer eyJhbGciOiJIUzI1NiJ9.NWE5MzA4ZjQ2ZmQwNzUwODQ2ZTIwMWI3._yHzrwVARzjph778qnt0YAUNTzPPHdSuOpxAhI8tAJ4');
+        cr.run((err, res) => {
+            if (err) {
+                console.log(err.message);
+            } else {
+                console.log(res);
+            }
+        });
+    });
 
 program
     .command('register <firstname> <lastname> <username> <password>')
